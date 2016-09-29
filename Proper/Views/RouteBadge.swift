d6682b1ab5b13eee76f7dd6ce6eaf1c4fb573c23
Elliott Williams <e@elliottwillia.ms>
2016-09-29 17:35:49 -0400

    Refactored RouteBadge to be nib-based, respond to layout changes, and support highlighting
    This fixes a bug where the badge would be drawn larger than its container if
    the container resized. This was revealed while working on #5. It's especially
    problematic in constraint-based layouts, where layout contraints are applied
    after the badge is initialized. The key to this fix involves invoking redraw
    code in `layoutSubviews`, and ensuring that the bounding rectangles of
    sublayers of the badge view (where the actual drawing is done) always match
    their parent container.
    
    I took the opportunity to clean up some of the messy UI code in this class. I
    removed all implicitly unwrapped optionals, reduced prevalence of redundant
    calculations and redraws, and removed some unnecessary abstraction.
    
    The new badge UI is loaded a xib (RouteBadge.xib), which allows us to set
    layout constraints on badges the Apple-preferred way (i.e. not
    programmatically), and do things like ensure the badge is always a 1:1 size.
    The badge view itself now always lives in a container, which takes the shape of
    its parent. This means that the view that the badge layers are attached to can
    be sized appropriately, regardless of the size its container is asked to take.
    The badge label, too, now adjusts its margins based on badge size and
    dynamically resizes its text to fit available space and this is set in the xib
    and no longer in draw code.
    
    Custom UI is complicated.

--- Proper/Views/RouteBadge.swift
+++ Proper/Views/RouteBadge.swift
@@ -6,189 +6,178 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class RouteBadge: UIView {
+
+    // The nib loaded in setup() provides three views:
+    // - container, which fills the bounds of self
+    @IBOutlet var container: UIView!
+    // - badge, which maintains a 1:1 aspect ratio and which holds the badge shape layers
+    @IBOutlet var badge: UIView!
+    // - label, which floats above badge to display the route number
+    @IBOutlet var label: UILabel!
     
     // MARK: Properties
-    
-    var capacity = 0.0 {
-        didSet { updateCapacity() }
+
+    // Properties that redraw the badge upon modification.
+    var capacity: CGFloat = 1.0 {
+        didSet { animateCapacityMask(from: oldValue, to: capacity) }
     }
-    var outerStrokeWidth = 0.0 {
-        didSet { updateStroke() }
+    var color = UIColor.grayColor() {
+        didSet { redraw() }
     }
-    var outerStrokeGap = 0.0 {
-        didSet { updateStroke() }
+    var strokeColor = UIColor.whiteColor() {
+        didSet {
+            label.textColor = strokeColor
+            redraw()
+        }
     }
+    var outerStrokeWidth: CGFloat = 0.0 {
+        didSet { redraw() }
+    }
+    var outerStrokeGap: CGFloat = 0.0 {
+        didSet { redraw() }
+    }
+
+    // A proxy to the text label contained within the badge.
     var routeNumber: String? {
-        didSet { updateRouteNumber() }
+        get { return label.text }
+        set { label.text = newValue }
     }
-    var color: UIColor = UIColor.lightGrayColor() {
-        didSet { updateColor() }
-    }
+
+    // Controls the highlight state of the badge, animating between states.
     var highlighted: Bool = false {
-        didSet { highlightChanged(from: oldValue, to: highlighted) }
+        didSet { changedHighlight(from: oldValue, to: highlighted) }
     }
 
     // MARK: Private properties
     
-    private let outerBadge = CAShapeLayer()
-    private let innerBadge = CAShapeLayer()
-    private let filledInnerBadge = CAShapeLayer()
+    private let outerLayer = CAShapeLayer()
+    private let innerLayer = CAShapeLayer()
+    private let filledInnerLayer = CAShapeLayer()
     private let capacityCoverMask = CAShapeLayer()
-    private var label: UILabel?
-    private var lightColor = UIColor(red: 1, green: 188/255, blue: 188/255, alpha: 1)
-    private var strokeColor = UIColor.lightTextColor()
 
-    private var width: CGFloat!
-    private var outerRadius: CGFloat!
-    private var innerWidth: CGFloat!
-    private var innerRadius: CGFloat!
-    private var centerX: CGFloat!
-    private var centerY: CGFloat!
-    private var capacityCover: CGFloat!
-    
     // MARK: - Initializers
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        // Calculate drawing measurements and draw the badge layers
-        setColors()
-        calculateDrawingMeasurements()
-        drawBadge()
-        insertLayers()
-        insertLabel()
+        setup()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setColors()
-        calculateDrawingMeasurements()
-        drawBadge()
-        insertLayers()
-        insertLabel()
+        setup()
+    }
+
+    func setup() {
+        // Load, configure, and attach the badge nib.
+        NSBundle.mainBundle().loadNibNamed("RouteBadge", owner: self, options: nil)
+        container.frame = self.bounds
+        addSubview(container)
+
+        // Apply constraints to the view hierarchy before drawing to the layers.
+        layoutIfNeeded()
+
+        // Configure the label to stack above the badge shapes and be the stroke color.
+        label.layer.zPosition = 15
+        label.textColor = strokeColor
+
+        // Insert shape layers.
+        [outerLayer, filledInnerLayer, innerLayer].forEach { layer in
+            badge.layer.addSublayer(layer)
+        }
+
+        // Clear the background placeholder colors.
+        backgroundColor = .clearColor()
+        badge.backgroundColor = .clearColor()
+
+        // Draw the badge in the next update cycle.
+        setNeedsLayout()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        container.layer.frame = self.bounds
+        badge.layer.frame = container.bounds
+        [outerLayer, filledInnerLayer, innerLayer].forEach { layer in
+            layer.frame = badge.layer.bounds
+        }
+        redraw()
     }
     
-    // MARK: Property-setting calculations
+    // MARK: Shape calculations
     
-    func setColors() {
-        backgroundColor = UIColor.clearColor()
-        lightColor = color.lightenedColor(0.8)
-        strokeColor = UIColor.snowColor()
-        
-        label?.textColor = strokeColor
-    }
-    
-    func calculateDrawingMeasurements() {
-        width = bounds.width
-        
-        innerWidth = CGFloat(width - 2 * CGFloat(outerStrokeWidth + outerStrokeGap))
-        innerRadius = (innerWidth / 2) - CGFloat(outerStrokeWidth/2)
-        centerX = width / 2
-        centerY = centerX
-        
-        outerRadius = (width / 2) - CGFloat(outerStrokeWidth)
-        capacityCover = innerWidth * CGFloat(1 - capacity)
-    }
-    
-    // MARK: Idempotent calculations
-    
-    func outerPath() -> CGMutablePath {
+    func outerPath(radius: CGFloat) -> CGMutablePath {
         let outer = CGPathCreateMutable()
-        CGPathAddArc(outer, nil, centerX, centerY, outerRadius, 0, CGFloat(2*M_PI), true)
+        CGPathAddArc(outer, nil, outerLayer.bounds.midX, outerLayer.bounds.midY, radius, 0, CGFloat(2*M_PI), true)
         return outer
     }
     
-    func innerPath() -> CGMutablePath {
+    func innerPath(radius: CGFloat) -> CGMutablePath {
         let inner = CGPathCreateMutable()
-        CGPathAddArc(inner, nil, centerX, centerY, innerRadius, 0, CGFloat(2*M_PI), true)
+        CGPathAddArc(inner, nil, innerLayer.bounds.midX, innerLayer.bounds.midY, radius, 0, CGFloat(2*M_PI), true)
         return inner
     }
     
-    func capacityCoverPath(height: CGFloat) -> CGPath {
-        return CGPathCreateWithRect(CGRectMake(0, CGFloat(outerStrokeWidth + outerStrokeGap), width, height), nil)
+    func capacityRect(amount: CGFloat) -> CGPath {
+        let height = (1 - amount) * innerLayer.bounds.height
+        let rect = CGRect(x: 0, y: outerStrokeWidth + outerStrokeGap, width: innerLayer.bounds.width, height: height)
+        return CGPathCreateWithRect(rect, nil)
     }
     
-    func labelMeasurements() -> (margin: CGFloat, size: CGFloat, fontSize: CGFloat) {
-        return (margin: width * 0.1, size: width * 0.8, fontSize: innerWidth * 0.55)
-    }
-    
-    // MARK: Setup draw code
-    
-    func insertLayers() {
-        layer.addSublayer(outerBadge)
-        layer.addSublayer(filledInnerBadge)
-        layer.addSublayer(innerBadge)
-    }
-    
-    func insertLabel() {
-        let (margin, size, fontSize) = labelMeasurements()
-        let label = UILabel(frame: CGRectMake(margin, margin, size, size))
-        label.font = UIFont.systemFontOfSize(fontSize, weight: UIFontWeightSemibold)
-        label.textColor = UIColor.whiteColor()
-        label.textAlignment = NSTextAlignment.Center
-        label.text = routeNumber
-        addSubview(label)
-        self.label = label
-    }
-    
-    // MARK: Reusable draw code
-    
-    func drawBadge() {
-        calculateDrawingMeasurements()
+    // MARK: Draw code
+
+    func redraw() {
+        let edge = min(badge.bounds.width, badge.bounds.height)
+        let innerWidth = edge - 2 * (outerStrokeWidth + outerStrokeGap)
+        let innerRadius = innerWidth / 2 - outerStrokeWidth / 2
+        let outerRadius = edge / 2 - outerStrokeWidth
+
+        outerLayer.path = outerPath(outerRadius)
+        outerLayer.strokeColor = strokeColor.CGColor
+        outerLayer.fillColor = color.CGColor
+        outerLayer.lineWidth = outerStrokeWidth
         
-        outerBadge.path = outerPath()
-        outerBadge.strokeColor = color.CGColor
-        outerBadge.fillColor = strokeColor.CGColor
-        outerBadge.lineWidth = CGFloat(outerStrokeWidth)
-        
-        capacityCoverMask.path = capacityCoverPath(capacityCover)
+        capacityCoverMask.path = capacityRect(capacity)
         capacityCoverMask.fillColor = UIColor.blackColor().CGColor
         
-        innerBadge.path = innerPath()
-        innerBadge.fillColor = color.CGColor
-        innerBadge.mask = capacityCoverMask
+        innerLayer.path = innerPath(innerRadius)
+        innerLayer.fillColor = UIColor.blackColor().colorWithAlphaComponent(0.5).CGColor
+        innerLayer.mask = capacityCoverMask
         
-        filledInnerBadge.path = innerPath()
-        filledInnerBadge.fillColor = lightColor.CGColor
-    }
-    
-    func updateCapacity() {
-        calculateDrawingMeasurements()
-        let anim = CABasicAnimation(keyPath: "path")
-        let path = capacityCoverPath(capacityCover)
-        anim.fromValue = capacityCoverMask.path
-        anim.toValue = path
-        anim.duration = 0.25
-        capacityCoverMask.addAnimation(anim, forKey: "path")
-        capacityCoverMask.path = path
-    }
-    
-    func updateStroke() {
-        drawBadge()
-        // Label font size is dependent on inner width
-        let fontSize = labelMeasurements().fontSize
-        label?.font = label?.font.fontWithSize(fontSize)
-    }
-    
-    func updateRouteNumber() {
-        label?.text = routeNumber
-    }
-    
-    func updateColor() {
-        setColors()
-        outerBadge.strokeColor = color.CGColor
-        outerBadge.fillColor = strokeColor.CGColor
-        innerBadge.fillColor = color.CGColor
-        filledInnerBadge.fillColor = color.CGColor
+        filledInnerLayer.path = innerPath(innerRadius)
+        filledInnerLayer.fillColor = color.CGColor
     }
 
-    func highlightChanged(from wasHighlighted: Bool, to highlighted: Bool) {
-        guard wasHighlighted != highlighted else { return }
-        UIView.animateWithDuration(0) {
-            self.color = highlighted ? self.color.darkenedColor(2/5) : self.color.lightenedColor(2/3)
-        }
+    // MARK: Animations
+    
+    func animateCapacityMask(from oldValue: CGFloat, to value: CGFloat) {
+        let anim = CABasicAnimation(keyPath: "path")
+        anim.fromValue = capacityRect(oldValue)
+        anim.toValue = capacityRect(value)
+        anim.duration = 0.25
+        capacityCoverMask.addAnimation(anim, forKey: "path")
+
+        redraw()
     }
-   
+
+    func changedHighlight(from wasHighlighted: Bool, to shouldHighlight: Bool) {
+        // Ensure the highlight state changed.
+        guard wasHighlighted != shouldHighlight else { return }
+
+        // Determine color keyframes for the animation by darkening the badge color, or re-lightening it.
+        let color = self.color
+        let darkened = self.color.darkenedColor(2/5)
+
+        // Avoid the implicit animation by making the color changes in an explicit transaction.
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        [outerLayer, filledInnerLayer].forEach { layer in
+            layer.fillColor = shouldHighlight ? darkened.CGColor : color.CGColor
+        }
+        label.textColor = shouldHighlight ? .lightGrayColor() : .whiteColor()
+        CATransaction.commit()
+    }
+
 }
