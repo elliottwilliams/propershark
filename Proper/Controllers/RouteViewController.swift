802a988a07923dd1f0c7aa625a066e7ca3914e79
Elliott Williams <e@elliottwillia.ms>
2016-10-13 09:02:58 -0400

    Use an independent navigation bar for custom styling.
    Unlike the implementation of #24, which animates attribute changes on one
    navigation bar shared by the application, this commit handles the same problem
    by creating an independent navigation bar for view controller scenes that want
    to customize the bar (in this case, RouteViewController).
    
    The benefits of this approach are that animation works smoothly and as expected
    (no awkward transitions), and that there's less of a risk of a view controller
    screwing up the global navigation bar by not unsetting some changes. The
    challenges of this approach are that we lose all of the stuff Proper gets "for
    free" from Apple's UINavigationController when we're in a view with a custom
    navigation bar. We need to manually wire the "back" button to move back (which
    this commit does), and we need to manually configure an edge pan gesture
    recognizer and a transition animation to get the same back-on-edge-swipe
    behavior (which this commit does not do).
    
    I'm committing this so that if some future Proper *needs* to use multiple
    independent navigation bars, we can draw from these initial experiments.

--- Proper/Controllers/RouteViewController.swift
+++ Proper/Controllers/RouteViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 import ReactiveCocoa
 
-class RouteViewController: UIViewController, ProperViewController, MutableModelDelegate {
+class RouteViewController: UIViewController, ProperViewController, MutableModelDelegate, UINavigationBarDelegate {
 
     var route: MutableRoute!
 
     // MARK: UI references
     @IBOutlet weak var badge: RouteBadge!
     @IBOutlet weak var infoContainer: UIView!
-    @IBOutlet weak var navItem: TransitNavigationItem!
+    @IBOutlet weak var navBar: UINavigationBar!
 
     // MARK: Internal properties
     internal var connection: ConnectionType = Connection.sharedInstance
+    internal let contrastingColor = MutableProperty<UIColor?>(nil)
     internal let disposable = CompositeDisposable()
+    internal let navItem: UINavigationItem = TransitNavigationItem()
 
     // MARK: Methods
     override func viewDidLoad() {
@@ -34,11 +36,12 @@ class RouteViewController: UIViewController, ProperViewController, MutableModelD
         badge.routeNumber = route.shortName
         disposable += route.color.producer.ignoreNil().startWithNext { color in
             let contrasting = color.blackOrWhiteContrastingColor()
+            self.contrastingColor.swap(contrasting)
             self.badge.color = color
             self.badge.strokeColor = contrasting
 
             // Store the current navigation bar style and adjust color to match route color.
-            self.colorNavigationBar(color)
+            self.styleNavigationBar()
             self.infoContainer.backgroundColor = color
         }
         disposable += route.name.producer.ignoreNil().startWithNext { name in
@@ -61,16 +64,9 @@ class RouteViewController: UIViewController, ProperViewController, MutableModelD
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
 
-        // After transitioning, reset the navigation bar to its default style.
-        transitionCoordinator()?.animateAlongsideTransition(nil, completion: { context in
-            UIView.animateWithDuration(animated ? 0.2 : 0.0) {
-                let vc = context.viewControllerForKey(UITransitionContextToViewControllerKey)
-                if let bar = vc?.navigationController?.navigationBar {
-                    RouteViewController.resetNavigationBar(bar)
-                    bar.layoutIfNeeded()
-                }
-            }
-        })
+        // Restore navigation bar
+        navigationController?.navigationBar.barStyle = .Default
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
     override func viewDidDisappear(animated: Bool) {
@@ -78,12 +74,40 @@ class RouteViewController: UIViewController, ProperViewController, MutableModelD
         super.viewDidDisappear(animated)
     }
 
-    func colorNavigationBar(color: UIColor) {
-        let contrasting = color.blackOrWhiteContrastingColor()
-        navigationController?.navigationBar.tintColor = contrasting
-        navigationController?.navigationBar.barTintColor = color
-        navigationController?.navigationBar.shadowImage = nil
-        navigationController?.navigationBar.barStyle = contrasting == UIColor.whiteColor() ? .Black : .Default
+    override func viewWillAppear(animated: Bool) {
+        // Hide the global navigation bar.
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+
+        // Use the state of the global navigation bar to form the "back" item in our custom navigation bar.
+        if let prevItem = navigationController?.navigationBar.topItem {
+            navBar.pushNavigationItem(UINavigationItem(title: prevItem.title ?? "Back"), animated: animated)
+        }
+
+        // Add the route's navigation item to the bar, and style the bar appropriately.
+        navBar.pushNavigationItem(navItem, animated: animated)
+        styleNavigationBar()
+    }
+
+    func styleNavigationBar() {
+        guard let color = route.color.value, let contrasting = contrastingColor.value else {
+            return
+        }
+
+        navBar.tintColor = contrasting
+        navBar.barTintColor = color
+
+        let barStyle = contrasting == UIColor.whiteColor() ? UIBarStyle.Black : UIBarStyle.Default
+        navBar.barStyle = barStyle
+        navigationController?.navigationBar.barStyle = barStyle
+    }
+
+    func navigationBar(navigationBar: UINavigationBar, shouldPopItem item: UINavigationItem) -> Bool {
+        if item == navItem {
+            navigationController?.popViewControllerAnimated(true)
+            return false
+        } else {
+            return true
+        }
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
